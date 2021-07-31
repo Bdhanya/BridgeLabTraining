727aede760d33599d6ca239cf2ef2b1743e179c3
Bdhanya <48754399+Bdhanya@users.noreply.github.com>
2021-07-31 08:35:43 +0530

    Code changes to add User Validation method

--- src/main/java/com/main/UserMain.java
+++ src/main/java/com/main/UserMain.java
@@ -1,8 +1,16 @@
 package com.main;
 
+import com.interfaces.UserServices;
+import com.model.User;
+import com.service.UserServiceImpl;
+
 public class UserMain {
     public static void main(String[] args) {
         System.out.println("User Class !!!");
+        User user = new User("Bharath", "Dhanya", "bharathsdhanya@gmail.com",
+                "91 9965433243", "3Rq3p6543E@");
+        UserServices userServices=new UserServiceImpl();
+        System.out.println("User Validation result:::: "+ userServices.validateUser(user));
     }
 
 }
